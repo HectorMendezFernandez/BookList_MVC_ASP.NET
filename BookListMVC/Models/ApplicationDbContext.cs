76978113673e8f6080d5aa4dd6914113c9e2ab35
Héctor <hector.mendez.fernandez02@gmail.com>
2022-07-16 05:37:52 -0600

    add databaseConnection with sqlserver

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        //agregamos el modelo a la base de datos
+        public DbSet<Book> MyProperty { get; set; }
+    }
+}
